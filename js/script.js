653105ef0d50341d35e28d5accfabe20d3354ea0
Owlynn <contact@jessalynn.fr>
2020-11-30 19:19:37 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -10,38 +10,36 @@ const app = {
     init () {
         console.log("Youpi");
         app.activateTriggers();
-        
+        app.clearSequences() 
     },
     
     activateTriggers () {
-        document.querySelectorAll(".trigger").forEach(
-            function(e){
-            e.addEventListener("click", app.createRobotSequence)
-            }
-        )
-    },
-    createRobotSequence(){
-        app.playerMessage.innerHTML="Concentrez vous et retenez l'odre d'apparition des couleurs...";
-        // app.playerMessage.visibility="visible";
-        for (let i = 0; i < 4; i++) {
-            let randomColor = app.availableColors[Math.floor(Math.random() * app.availableColors.length)]
-            app.robotSequence.push(randomColor)
-        }
-        console.log(app.robotSequence);
-        // app.startGame();
-    },
-    createUserSequence(event){
-       let currentTrigger = event.target;
-       app.robotSequence.push(currentTrigger.classList[1])
-       console.log(app.robotSequence);
+        document.querySelector(".start-game").addEventListener("click", app.createRobotSequence);
     },
     startGame (){
         let startButton = document.querySelector(".start-game");
         startButton.onclick = app.createRobotSequence();
     },
+    createRobotSequence(){
+        app.playerMessage.innerHTML="Concentrez vous et retenez l'odre d'apparition des couleurs...";
+        app.playerMessage.style.visibility="visible";
+            for (let i = 0; i < 4; i++) {
+                let randomColor = app.availableColors[Math.floor(Math.random() * app.availableColors.length)]
+                app.robotSequence.push(randomColor)
+            }
+        console.log(app.robotSequence);
+        // app.createUserSequence();
+    },
+    // createUserSequence(event){
+    //    app.playerMessage.innerHTML="C'est à votre tour !";
+    //    let currentTrigger = event.target;
+    //    app.userSequence.push(currentTrigger.classList[1])
+    //    console.log(app.robotSequence);
+    // },
     clearSequences() {
         app.robotSequence = []
         app.userSequence = []
+        app.playerMessage.style.visibility="hidden";
     }
 };
 
