6f4eaa221be3fefd84faf11df327059b6d88d932
Owlynn <contact@jessalynn.fr>
2020-11-30 17:59:00 +0100

    createRobotSequence 1

--- js/script.js
+++ js/script.js
@@ -3,24 +3,38 @@ const app = {
 // PROPERTIES
     robotSequence : [],
     userSequence : [],
+    availableColors : ["red", "green", "blue","yellow"],
 
 // METHODS
     init () {
         console.log("Youpi");
+        app.activateTriggers();
+        
+    },
+    
+    activateTriggers () {
         document.querySelectorAll(".trigger").forEach(
             function(e){
             e.addEventListener("click", app.createRobotSequence)
             }
         )
-       
     },
-    createRobotSequence(event){
+    createUserSequence(event){
        let currentTrigger = event.target;
        console.log(currentTrigger.classList[1])
-       
+       app.robotSequence.push(currentTrigger.classList[1])
+       console.log(app.robotSequence);
+        // if(app.robotSequence.length > 3)
+        // app.robotSequence = [];
+        // }
+    },
+    createRobotSequence(){
+        for (let i = 0; i < 4; i++) {
+            let randomColor = app.availableColors[Math.floor(Math.random() * app.availableColors.length)]
+            app.robotSequence.push(randomColor)
+            console.log(app.robotSequence);
+        }
     }
-  
-       
 };
 
 document.addEventListener('DOMContentLoaded', app.init);
