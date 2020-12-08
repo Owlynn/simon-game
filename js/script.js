b0e0d614ab92049b8f443d1b80815ec7c0bbe844
Owlynn <contact@jessalynn.fr>
2020-12-08 16:18:15 +0100

    Mise en place de l'affichange des couleurs en séquentiel

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const app = {
     userSequence : [],
     availableColors : ["red", "green", "blue","yellow"],
     playerMessage : document.querySelector(".player-message"),
+    maxColors : 4,
 
 // METHODS
     init () {
@@ -20,30 +21,51 @@ const app = {
         let startButton = document.querySelector(".start-game");
         startButton.onclick = app.createRobotSequence();
     },
-    createRobotSequence(){
+    createRobotSequence() {
         app.playerMessage.innerHTML="Concentrez vous et retenez l'odre d'apparition des couleurs...";
         app.playerMessage.style.visibility="visible";
-
+    
         // chooses 4 random colors and puts it into an array
-
+    
             for (let i = 0; i < 4; i++) {
-                let randomColor = app.availableColors[Math.floor(Math.random() * app.availableColors.length)]
-                app.robotSequence.push(randomColor)
+                let randomColor = app.availableColors[Math.floor(Math.random() * app.availableColors.length)];
+                app.robotSequence.push(randomColor);
             }
 
+    
             console.log("tableau de random " + app.robotSequence);
+    
+            // app.robotSequence.forEach( (currentColor) => {
+            //     document.querySelector("." + currentColor).classList.add(currentColor + ("--active"));
+            
+            //     setTimeout(
+                //         function() {
+                    //             document.querySelector("." + currentColor).classList.remove(currentColor + "--active");
+                    //         }
+                    //         ,3000
+                    //     );
+                    // });
 
-            app.robotSequence.forEach((e) => {
-                // console.log("forEach current e " + e);
-                document.querySelector("." + e).classList.add(e + ("--active"))
-
-                    setTimeout(
-                        function() {
-                            document.querySelector("." + e).classList.remove(e + "--active");
-                        }
-                        ,3000
-                    );
-            };
+        let currentIndex = 0;
+        // let currentColor = app.robotSequence[currentIndex];
+        console.log({currentIndex});
+        console.log("couleur en cours : "+app.robotSequence[currentIndex]);
+        
+        setInterval(() => {
+                document.querySelector("." + app.robotSequence[currentIndex]).classList.add(app.robotSequence[currentIndex] + ("--active"));
+                console.log("j'active la couleur : " + app.robotSequence[currentIndex]);
+            }, 3000
+            );
+            
+            setInterval(
+                function() {
+                    document.querySelector("." + app.robotSequence[currentIndex]).classList.remove(app.robotSequence[currentIndex] + "--active");
+                    console.log("je désactive la couleur : " + app.robotSequence[currentIndex]);
+                    currentIndex++
+                }
+                ,4000
+                );       
+                
     },
 
     clearSequences() {
