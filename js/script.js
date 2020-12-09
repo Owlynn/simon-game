80f05652bee57a6a6f8dec8f06ef8a912b4943f7
Owlynn <contact@jessalynn.fr>
2020-12-09 15:01:44 +0100

    Ajout de la fonction check if right et réglage du bug d'afichange des messages

--- js/script.js
+++ js/script.js
@@ -4,74 +4,102 @@ const app = {
     robotSequence : [],
     userSequence : [],
     availableColors : ["red", "green", "blue","yellow"],
-    playerMessage : document.querySelector(".player-message"),
+    playerMessage1 : document.querySelector(".player-message"),
+    playerMessage2 : document.querySelector(".player-message-2"),
+    startButton : document.querySelector(".start-game"),
     maxColors : 4,
-
-// METHODS
+    
+    // METHODS
     init () {
         console.log("init ok");
-        app.activateTriggers();
-        app.clearSequences() 
+        // app.createUserSequence();
+        // app.clearSequences() 
+        app.startGame();
+        
     },
     
-    activateTriggers () {
-        document.querySelector(".start-game").addEventListener("click", app.createRobotSequence);
-    },
     startGame (){
-        let startButton = document.querySelector(".start-game");
-        startButton.onclick = app.createRobotSequence();
+        app.clearSequences();
+        app.startButton.addEventListener("click", app.createRobotSequence)
     },
     createRobotSequence() {
-        app.playerMessage.innerHTML="Concentrez vous et retenez l'odre d'apparition des couleurs...";
-        app.playerMessage.style.visibility="visible";
-    
+        
+        app.playerMessage1.innerHTML="Concentrez vous et retenez l'odre d'apparition des couleurs...";
+        app.playerMessage1.style.display = "block"
+        app.startButton.style.display = "none";
+        
         // chooses random colors and puts it into an array
-    
-            for (let i = 0; i < 4; i++) {
-                let randomColor = app.availableColors[Math.floor(Math.random() * app.availableColors.length)];
-                app.robotSequence.push(randomColor);
-            }
-
-    
-            console.log("tableau de random " + app.robotSequence);
-
+        
+        for (let i = 0; i < 4; i++) {
+            let randomColor = app.availableColors[Math.floor(Math.random() * app.availableColors.length)];
+            app.robotSequence.push(randomColor);
+        }
+        
+        console.log("tableau du robot (robot sequence) " + app.robotSequence);
+        
         let currentIndex = 0;
-        console.log("couleur en cours : "+app.robotSequence[currentIndex]);
         
         setInterval(
             function () {
                 if (currentIndex < app.maxColors) {
-                    console.log({currentIndex});
                     document.querySelector("." + app.robotSequence[currentIndex]).classList.add(app.robotSequence[currentIndex] + ("--active"));
-                    console.log("j'active la couleur : " + app.robotSequence[currentIndex]);
                 }
             }, 1000
-        );
+            );
             
-        setInterval(
-            function() {
-                if (currentIndex < app.maxColors) {
-                    document.querySelector("." + app.robotSequence[currentIndex]).classList.toggle(app.robotSequence[currentIndex] + "--active");
-                    console.log("je désactive la couleur : " + app.robotSequence[currentIndex]);
-                    currentIndex++
+            let deactivateClass = setInterval(
+                function() {
+                    if (currentIndex < app.maxColors) {
+                        document.querySelector("." + app.robotSequence[currentIndex]).classList.toggle(app.robotSequence[currentIndex] + "--active");
+                        currentIndex++
+                    }
+                    if (currentIndex > app.maxColors-1) {
+                        document.addEventListener("mousedown",app.createUserSequence)
+                        app.playerMessage1.innerHTML = "C'est à votre tour ! Reproduisez la séquence de couleurs"
+                        clearInterval(deactivateClass)
+                    }
                 }
-            }
-            , 1100
-        );       
+                , 1100
+                );
+                
+            },
+            
+            createUserSequence(event){
+                let currentTrigger = event.target;
+                console.log("longueur: " + app.userSequence.length);
+                console.log("max colors " + app.maxColors)
+                
+                if (app.userSequence.length < app.maxColors ) {
+                    if (currentTrigger.classList[1] !== undefined) {
+                        console.log("length inférieur à max colors et classlist existe");
+                        app.userSequence.push(currentTrigger.classList[1])
+                    }
+        }
+        if(app.userSequence.length == app.maxColors){
+            console.log("length supérieur à max colors");
+            app.checkIfRight();
+        }
+        
     },
-
+    checkIfRight() {
+        let robotString = app.robotSequence.toString();
+        let userString = app.userSequence.toString();
+        
+        if(robotString == userString ) {
+            app.playerMessage1.innerHTML = "Bravo vous avez gagné !"
+        } 
+        if(robotString != userString){
+            app.playerMessage1.innerHTML = "C'est perdu !";
+            app.startButton.style.display = "block";
+            
+        }
+        app.startButton.onclick = app.startGame();
+    },
     clearSequences() {
         app.robotSequence = []
         app.userSequence = []
-        app.playerMessage.style.visibility="hidden";
+        // this.startButton.innerHTML = "Commencer la partie"
     }
 };
 
 document.addEventListener('DOMContentLoaded', app.init);
-
-// createUserSequence(event){
-//    app.playerMessage.innerHTML="C'est à votre tour !";
-//    let currentTrigger = event.target;
-//    app.userSequence.push(currentTrigger.classList[1])
-//    console.log(app.robotSequence);
-// },
